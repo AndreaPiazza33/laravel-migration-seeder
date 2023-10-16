ad6db83a0254b01c4ac941593f1aed4fb4bc5a60
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-10-16 17:30:04 +0200

    feat: add migration

---
+++ database/migrations/2023_10_16_143119_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda', 50);
+            $table->string('Stazione di partenza', 50);
+            $table->string('Stazione di arrivo', 50);
+            $table->date('Orario di partenza');
+            $table->date('Orario di arrivo');
+            $table->bigInteger('Codice treno');
+            $table->smallInteger('Numero di carrozze')->unsigned();
+            $table->boolean('In orario');
+            $table->boolean('Cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
